fa0c49439658d5376a481da3c4611db75ea067c8
Chris Spurlock <cjs7w6@gmail.com>
2016-11-28 15:57:25 -0600

    first tab of Insurance 101 done

--- insurance101.php
+++ insurance101.php
@@ -22,6 +22,56 @@
                             <span>Doing What's Right</span>
                         </a>
                     </div>
+                    <div id="the_basics" class="page">
+                        <div id="basics_intro">
+                            <div class="textarea col-md-6">
+                                <h2>Accidents happen. Cars swerve. Zombies bite</h2>
+                                <p>Death isn’t a typical concern for young healthy people. Term life insurance is often the best option for protecting against the unexpected. It provides coverage for a specific period of time, and is designed to help your loved ones make up for the loss of your income if something were to happen to you.</p>
+                                <p>In doing so, it covers financial obligations that disappear over time, such as a mortgage, college tuition and loans. The healthier and younger you are, the more affordable coverage will be.</p>
+                            </div>
+                            <div class="imgarea col-md-6">
+                                <img src="resources/images/moving-chair.jpg">
+                            </div>
+                        </div>
+                        <div id="app_process">
+                            <div class="imgarea col-md-5">
+                                <img src="resources/images/ipad.jpg">
+                            </div>
+                            <div class="textarea col-md-7">
+                                <h2>The Application Process</h2>
+                                <p>Getting life insurance isn't difficult, but there are a few steps in the process.</p>
+                                <div id="nums">
+                                    <div class="single-num">
+                                        <span class="pinknum">
+                                            1
+                                        </span>
+                                        <div class="textwrapper">
+                                            <h3>Get a quote</h3>
+                                            <p>By filling out a short financial form, OUR LIFE COVERED<sup>SM</sup> will provide you with a personalized recommendation on the coverage you need.</p>
+                                        </div>
+                                    </div>
+                                    <div class="single-num">
+                                        <span class="pinknum">
+                                            2
+                                        </span>
+                                        <div class="textwrapper">
+                                            <h3>Apply for Coverage</h3>
+                                            <p>Identify yourself and your beneficiaries, and answer a few initial health questions. You will also undergo a simple medical exam that will help determine your eligibility and the cost of your coverage. Most applicants are approved.</p>
+                                        </div>
+                                    </div>
+                                    <div class="single-num">
+                                        <span class="pinknum">
+                                            3
+                                        </span>
+                                        <div class="textwrapper">
+                                            <h3>Receive your Policy</h3>
+                                            <p>Once approved, you will receive your calculated premium and your policy for acceptance and signing. After paying your first premium (annual, quarterly or monthly payments), the carrier will put your policy in force. At this point, your policy cannot be cancelled unless you fail to pay your premiums according to the provisions of the agreement.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

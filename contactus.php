8803a26a0917e7741c0c1e71ddb90c1e2ae78f71
Chris Spurlock <cjs7w6@gmail.com>
2016-12-22 14:53:38 -0600

    update style on Contact, Financial Ratings

--- contactus.php
+++ contactus.php
@@ -8,7 +8,7 @@
                     <h2 class="text-center">Contact Us</h2>
                 </div>
                 <div class="col-md-6 col-md-offset-3 margin-bottom-m">
-                   	<p id="contact-us-intro-text" class="text-center"></p> 
+                    <p id="contact-us-intro-text" class="text-center"></p> 
                     <p id="contact-us-intro-text" class="text-center">Fill out the form below and we'll be in touch.</p>
                     <p id="contact-us-privacy-text" class="text-center">Our Privacy Policy can be found below.</p>
                 </div>
@@ -16,7 +16,7 @@
                     <form action="" id="contact-form" name="contactForm">
                         <div class="error-text">                                
                         </div>
-                        <div class="success-text" style="font-size:150%">                                
+                        <div class="success-text" style="font-size:145%">                                
                         </div>
                         <span class="hide-contact-fields">
                             <div class="inline-text required">
@@ -35,9 +35,25 @@
                                 <input class=" " type="text" id="contact-form-best-time" name="contact-form-best-time" maxlength="25">
                                 <label class=" " for="contact-form-best-time">Best Time To Call</label>
                             </div>
+                            <div class="inline-select required">
+                                <select name="time-zone" class="small" id="contact-time-zone" required="">
+                                    <option value="">- Select a Time Zone -</option>
+                                    <option value="PT">Pacific Time</option>
+                                    <option value="CT">Central Time</option>
+                                    <option value="ET">Eastern Time</option>
+                                </select>
+                            </div>
                             <div class="">
                                 <label style="font-size:16px" for="contact-form-comments">Comments</label>
-                                <textarea id="contact-form-comments" name="contact-form-comments" rows="5" cols="55" maxlength="800"></textarea><grammarly-btn><div style="visibility: hidden; z-index: 2;" class="_9b5ef6-textarea_btn _9b5ef6-not_focused" data-grammarly-reactid=".0"><div class="_9b5ef6-transform_wrap" data-grammarly-reactid=".0.0"><div title="Protected by Grammarly" class="_9b5ef6-status" data-grammarly-reactid=".0.0.0">&nbsp;</div></div></div></grammarly-btn>
+                                <textarea id="contact-form-comments" name="contact-form-comments" rows="5" cols="55" maxlength="800"></textarea>
+                            </div>
+                            <div class="g-recaptcha" data-sitekey="6LdK6w4UAAAAANjpS275dBIEfBbY0-4iVGQW6N4n" data-callback="wow()">
+                                <div style="width: 304px; height: 78px;">
+                                    <div>
+                                        <iframe src="https://www.google.com/recaptcha/api2/anchor?k=6LdK6w4UAAAAANjpS275dBIEfBbY0-4iVGQW6N4n&amp;co=aHR0cHM6Ly9zdGcucG9saWN5aHViLmxpZmU6NDQz&amp;hl=en&amp;v=r20161215161951&amp;size=normal&amp;cb=8b00o177g4xs" title="recaptcha widget" width="304" height="78" frameborder="0" scrolling="no" name="undefined"></iframe>
+                                    </div>
+                                    <textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none;  display: none; "></textarea>
+                                </div>
                             </div>
                             <div class="text-center">
                                 <br>

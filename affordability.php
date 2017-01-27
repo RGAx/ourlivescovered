94378b129d14c7c3b858265a57ae6e67c63c0854
Chris Spurlock <cjs7w6@gmail.com>
2017-01-27 13:19:07 -0600

    style changes per Barry's note

--- affordability.php
+++ affordability.php
@@ -9,47 +9,56 @@
             </div>
             <div class="row">
                 <div class="col-md-12">
-                        <div class="form-wrapper">
-                            <div class="primary-container">
-                                <form>
-                                    <div class="select-wrapper">
-                                        <label for="affordablePremium">Preferred Payment</label>
-                                        <select name="affordablePremium" id="affordablePremium" class="coverageamt-select afford-dropdown">
-                                            <!-- <option value="">Amount</option> -->
-                                            <option value="twentyfive">$25 per month</option>
-                                            <option value="fifty">$40 per month</option>
-                                            <option value="onehundred">$50 per month</option>
-                                        </select>
-                                    </div>
-                                    <div class="select-wrapper">
-                                        <label for="affordableAge">Age Range</label>
-                                        <select name="affordableAge" id="affordableAge" class="coverageamt-select afford-dropdown">
-                                            <!-- <option value="">Age</option> -->
-                                            <option value="twentyfive">25-29</option>
-                                            <option value="thirty">30-34</option>
-                                            <option value="thirtyfive">35-39</option>
-                                            <option value="forty">40-44</option>
-                                            <option value="fortyfive">45-49</option>
-                                            <option value="fifty">50+</option>
-                                        </select>
-                                    </div>
-                                    <div class="select-wrapper">
-                                        <label for="affordableGender">Gender</label>
-                                        <select name="affordableGender" id="affordableGender" class="coverageamt-select afford-dropdown">
-                                            <!-- <option value="">Gender</option> -->
-                                            <option value="male">Male</option>
-                                            <option value="female">Female</option>
-                                        </select>
-                                    </div>
-                                </form>
+                    <div class="form-wrapper">
+                        <div class="primary-container">
+                            <form action="quote-form-affordability.do" method="POST" name="mainAffordableForm" id="main-affordable-form">
+                                <div class="inline-text required">
+                                    <label class=" " for="afford-full-name">Full Name</label>
+                                    <input required="" class=" " type="text" id="afford-full-name" name="first-name" maxlength="25">
+                                </div>
+                                <div class="inline-text required">
+                                    <label class=" " for="afford-email-address">Email Address</label>
+                                    <input required="" class="  " type="text" id="afford-email-address" name="email-address" maxlength="100">
+                                </div>
+                                <div class="select-wrapper">
+                                    <label for="affordablePremium">Preferred Payment</label>
+                                    <select name="affordablePremium" id="affordablePremium" class="coverageamt-select afford-dropdown">
+                                        <!-- <option value="">Amount</option> -->
+                                         <option value="twentyfive">$25 per month</option>
+                                         <option value="forty">$40 per month</option>
+                                         <option value="fifty">$50 per month</option>
+                                    </select>
+                                </div>
+                                <div class="select-wrapper">
+                                    <label for="affordableAge">Age Range</label>
+                                    <select name="affordableAge" id="affordableAge" class="coverageamt-select afford-dropdown">
+                                        <!-- <option value="">Age</option> -->
+                                        <option value="twentyfive">25-29</option>
+                                        <option value="thirty">30-34</option>
+                                        <option value="thirtyfive">35-39</option>
+                                        <option value="forty">40-44</option>
+                                        <option value="fortyfive">45-49</option>
+                                        <option value="fifty">50+</option>
+                                    </select>
+                                </div>
+                                <div class="select-wrapper">
+                                    <label for="affordableGender">Gender</label>
+                                    <select name="affordableGender" id="affordableGender" class="coverageamt-select afford-dropdown">
+                                        <!-- <option value="">Gender</option> -->
+                                        <option value="male">Male</option>
+                                        <option value="female" selected>Female</option>
+                                    </select>
+                                </div>
+                                <input type="hidden" id="hiddenCoverage" name="totalCoverage" class="quoted-amount" value="$1,200,000">
                             </div>
                             <div class="secondary-container">
-                                <p id="initialQuote">Based on the above, you may qualify for about <span id="totalCoverage" class="quoted-amount">$550,000</span> in 10-year term coverage, and save close to <span id="totalSavings" class="quoted-amount">$10</span> per month over standard premium rates.<sup>*</sup>
+                                <p id="initialQuote">Based on the above, you may qualify for about <span id="totalCoverage" class="quoted-amount">$1,200,000</span> in 10-year term coverage.<!-- , and save close to <span id="totalSavings" class="quoted-amount">$15</span> per month over standard premium rates.<sup>*</sup> -->
                             </div>
                             <div class="gobutton-container">
                                 <input type="button" class="btn btn-primary" value="Start My Quote" id="main-affordable-form" wpvalue="continue" onclick="return validatePremiumMlc();">
                             </div>
-                        </div>
+                        </form>
+                    </div>
                 </div>
             </div>
             <div class="row">
